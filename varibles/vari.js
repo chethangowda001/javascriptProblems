c236b0a5f7aed9cac24d90583a48b713bb37c90b
[chethangowda001] <[chehangowda001@gmail.com]>
2024-06-21 11:16:49 +0530

    new varible floder added

---
+++ varibles/vari.js
@@ -0,0 +1,3 @@
+// Declare a variable firstname and initialize it with the value 'Lata'.
+
+const firstname = "Lata";
---
+++ varibles/vari1_0.js
@@ -0,0 +1,7 @@
+// /Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
+
+
+const flower = "rose";
+const tree = "maple";
+
+console.log("favourite flower: "+ flower+'/n'+"favourite tree:"+ tree);
